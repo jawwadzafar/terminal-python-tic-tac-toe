59ddfb358f23c6e2c1caa89a6752744fb5e65c10
Jawwad Zafar <zafarjawwad@gmail.com>
2021-06-02 16:05:19 +0530

    change player

--- game.py
+++ game.py
@@ -11,34 +11,41 @@ class Game(object):
         print(self.board[3] + " | " + self.board[4] + " | " + self.board[5])
         print(self.board[6] + " | " + self.board[7] + " | " + self.board[8])
 
-    def turn(self, player):
-        print(player + "'s turn.")
-        #TODO: improve to show only available turns
-        #TODO: handle exception for non int numbers
+
+    def turn(self):
+        print(self.current_player + "'s turn.")
+        # TODO: improve to show only available turns
+        # TODO: handle exception for non int numbers
         position = input("Choose a position from 1 to 9 ")
         valid = False
         while not valid:
-            #TODO: use range instead of list
+            # TODO: use range instead of list
             while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
                 position = input("Incorrect. Choose a position from 1 to 9 again ")
 
-            position = int(position) - 1j
+            position = int(position) - 1
 
             if self.board[position] == "-":
                 valid = True
             else:
                 print("Choose another position.")
 
-        self.board[position] = player
+        self.board[position] = self.current_player
         self.display_board()
     
-
+    def change_player(self):
+        if self.current_player == "X":
+            self.current_player = "O"
+        else:
+            self.current_player = "X"
+        
     def run(self):
         print('Welcome to Terminal Tic Tac Toe')
         self.display_board()
 
         while self.game_still_going:
-            self.turn(self.current_player)
+            self.turn()
+            self.change_player()
 
 if __name__ == "__main__":
     print('Creating game object...')
