2453dfd531225659ea09bcc65f9b8c4378fc4aed
Jawwad Zafar <zafarjawwad@gmail.com>
2021-06-02 16:40:30 +0530

    clear screen on every turn

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from util import clear
 class Game(object):
     def __init__(self):
         self.board = ['-', '-', '-','-', '-', '-','-', '-', '-']
@@ -7,6 +8,7 @@ class Game(object):
 
 
     def display_board(self):
+        clear()
         print(self.board[0] + " | " + self.board[1] + " | " + self.board[2])
         print(self.board[3] + " | " + self.board[4] + " | " + self.board[5])
         print(self.board[6] + " | " + self.board[7] + " | " + self.board[8])
