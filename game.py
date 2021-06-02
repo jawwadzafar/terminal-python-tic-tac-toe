cf93fca6f96c646d41ed116ec98a5b662da161b4
Jawwad Zafar <zafarjawwad@gmail.com>
2021-06-02 16:30:30 +0530

    winning and tie logic

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 class Game(object):
     def __init__(self):
-        self.board = ['-', '-', '-','-', '-', '-','-', '-', '-',]
+        self.board = ['-', '-', '-','-', '-', '-','-', '-', '-']
         self.game_still_going = True
         self.winner = None
         self.current_player = "X"
@@ -38,6 +38,80 @@ class Game(object):
             self.current_player = "O"
         else:
             self.current_player = "X"
+
+    def check_rows(self):
+        row_1 = self.board[0] == self.board[1] == self.board[2] != "-"
+        row_2 = self.board[3] == self.board[4] == self.board[5] != "-"
+        row_3 = self.board[6] == self.board[7] == self.board[8] != "-"
+        #If any row has match, flag there's a win
+        if row_1 or row_2 or row_3:
+            self.game_still_going = False
+        #Return the winner (X or O)
+        if row_1:
+            return self.board[0]
+        elif row_2:
+            return self.board[3]
+        elif row_3:
+            return self.board[6]
+        return
+
+    def check_columns(self):
+        set_1 = self.board[0] == self.board[3] == self.board[6] != "-"
+        set_2 = self.board[1] == self.board[4] == self.board[7] != "-"
+        set_3 = self.board[2] == self.board[5] == self.board[8] != "-"
+        #If any row has match, flag there's a win
+        if set_1 or set_2 or set_3:
+            self.game_still_going = False
+        #Return the winner (X or O)
+        if set_1:
+            return self.board[0]
+        elif set_2:
+            return self.board[1]
+        elif set_3:
+            return self.board[2]
+        return
+
+    def check_diagonals(self):
+        #Check if any of these diagonals have the same value
+        diagonal_1 = self.board[0] == self.board[4] == self.board[8] != "-"
+        diagonal_2 = self.board[6] == self.board[4] == self.board[2] != "-"
+        #If any diagonal has match, flag there's a win
+        if diagonal_1 or diagonal_2 :
+            self.game_still_going = False
+        #Return the winner (X or O)
+        if diagonal_1:
+            return self.board[0]
+        elif diagonal_2:
+            return self.board[6]
+        return
+    def check_if_tie(self):
+        if "-" not in self.board:
+            self.game_still_going = False
+        return
+
+    def check_game_status(self):
+        #check tie
+        self.check_if_tie()
+        
+        #Check rows
+        row_winner = self.check_rows()
+
+        #Check columns
+        column_winner = self.check_columns()
+
+        #Check diagonals
+        diagonal_winner = self.check_diagonals()
+
+        if row_winner:
+            self.winner = row_winner
+        elif column_winner:
+            self.winner = column_winner
+        elif diagonal_winner:
+            self.winner = diagonal_winner
+        else:
+            self.winner = None
+
+        return
         
     def run(self):
         print('Welcome to Terminal Tic Tac Toe')
@@ -46,6 +120,13 @@ class Game(object):
         while self.game_still_going:
             self.turn()
             self.change_player()
+            self.check_game_status()
+
+        #End of the game
+        if self.winner == "X" or self.winner == "O":
+            print(self.winner + " won.")
+        elif self.winner == None:
+            print("Tie. ")
 
 if __name__ == "__main__":
     print('Creating game object...')
